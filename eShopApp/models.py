f17de4fcca9a75bb6e122dad5f93b768681c70aa
Namra  Abid <s.namraabid@gmail.com>
2023-03-03 10:56:46 +0500

    Made Product and Category model

--- eShopApp/models.py
+++ eShopApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name=models.CharField(max_length=100)
+    price=models.IntegerField(default=0)
+    description=models.CharField(max_length=300,default='')
+    image=models.ImageField(upload_to='uploads/prodcuts/')
+
+class Category(models.Model):
+    name=models.CharField(max_length=30)
