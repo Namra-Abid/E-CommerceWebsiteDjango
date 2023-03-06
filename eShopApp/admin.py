1028de443290a91a1a2c395990137549ce43da76
Namra  Abid <s.namraabid@gmail.com>
2023-03-06 16:10:03 +0500

    Set TableView of Product & Category

--- eShopApp/admin.py
+++ eShopApp/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
 from .models import Product, Category
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+
+
+class AdminProduct(admin.ModelAdmin):
+    """ In admin panel Product table will
+        now only show these three  fields
+    """
+    list_display=['name','price','Category']
+
+
+class AdminCategory(admin.ModelAdmin):
+    """ In admin panel Product table will
+        now only show these three  fields
+    """
+    list_display=['name']
+
+
+
+
+
+admin.site.register(Product,AdminProduct)
+admin.site.register(Category,AdminCategory)
--- eShopApp/models.py
+++ eShopApp/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name=models.CharField(max_length=30)
+    
+    def __str__(self) :
+        return self.name
+      
+
+
 class Product(models.Model):
     name=models.CharField(max_length=100)
     price=models.IntegerField(default=0)
+    Category=models.ForeignKey(Category,on_delete=models.CASCADE,default=1)
     description=models.CharField(max_length=300,default='')
     image=models.ImageField(upload_to='uploads/prodcuts/')
-
-class Category(models.Model):
-    name=models.CharField(max_length=30)
+    #def __str__(self) :
+     #   return self.name
