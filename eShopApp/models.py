f67005ee7d9739fc7afd6b7d5385faab6e9dbd9d
Namra  Abid <s.namraabid@gmail.com>
2023-03-06 17:03:42 +0500

    created templates & Static method in Product model

--- eShopApp/models.py
+++ eShopApp/models.py
@@ -13,7 +13,11 @@ class Product(models.Model):
     name=models.CharField(max_length=100)
     price=models.IntegerField(default=0)
     Category=models.ForeignKey(Category,on_delete=models.CASCADE,default=1)
-    description=models.CharField(max_length=300,default='')
+    description=models.CharField(max_length=300,default='',null=True,blank=True)
     image=models.ImageField(upload_to='uploads/prodcuts/')
     #def __str__(self) :
      #   return self.name
+    @staticmethod
+    def get_all_products():
+        return Product.objects.all()
+
--- eShopApp/views.py
+++ eShopApp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Product
 # Create your views here.
+def index(request):
+    products=Product.get_all_products()
+    print(products)
+    return HttpResponse("Hello")
