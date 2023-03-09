73be0dc6a77be7355398c1a3f6846acbfb3aad3c
Namra  Abid <s.namraabid@gmail.com>
2023-03-09 11:27:16 +0500

    Fucntionality to show image of product in card

--- EShopProject/settings.py
+++ EShopProject/settings.py
@@ -122,3 +122,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+
+"""Self added things below here"""
+
+MEDIA_URL='/images/download-product/'
+MEDIA_ROOT=BASE_DIR
