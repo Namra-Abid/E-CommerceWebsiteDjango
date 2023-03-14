72b08f45a640d8c36619ca98b428d9c6971634a7
Namra  Abid <s.namraabid@gmail.com>
2023-03-14 16:00:00 +0500

    Created right side bar for categories

--- eShopApp/models.py
+++ eShopApp/models.py
@@ -3,6 +3,9 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name=models.CharField(max_length=30)
+    @staticmethod
+    def get_all_categories():
+        return Category.objects.all()
     
     def __str__(self) :
         return self.name
--- eShopApp/views.py
+++ eShopApp/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Product
+from .models import Product,Category
 # Create your views here.
 def index(request):
     products=Product.get_all_products()
-    print(products)
-    return render(request,'eShopApp/index.html',{'products':products})
+    categories=Category.get_all_categories()
+    content={
+        'products':products,
+        'categories':categories
+    }
+    #print(products)
+    return render(request,'eShopApp/index.html',content)
