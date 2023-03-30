d8163a00a84c5e288a7bbcde11601b1af61b97e5
Namra  Abid <s.namraabid@gmail.com>
2023-03-30 11:56:26 +0500

    Created filter/functionality to show cart_count of product

--- eShopApp/templatetags/cartsfilter.py
+++ eShopApp/templatetags/cartsfilter.py
@@ -9,5 +9,14 @@ def is_in_cart(product,cart):
         if int(id)==product.id:
             return True
     return False
+@register.filter(name='count_in_cart')
+def count_in_cart(product,cart):
+    keys=cart.keys()
+    print(product,cart)
+    for id in keys:
+        if int(id)==product.id:
+            return cart.get(id)
+    return False
+
 
 
