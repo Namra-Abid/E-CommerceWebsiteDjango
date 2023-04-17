ea1c3eb99a49f70c9d39a5b499444a0ff64a2451
Namra  Abid <s.namraabid@gmail.com>
2023-04-17 09:26:32 +0500

    minor changes in cart UI

--- eShopApp/templatetags/cartsfilter.py
+++ eShopApp/templatetags/cartsfilter.py
@@ -19,7 +19,7 @@ def is_in_cart(product,cart):
 @register.filter(name='count_in_cart')
 def count_in_cart(product,cart):
     keys=cart.keys()
-    print(product,cart)
+    #print(product,cart)
     for id in keys:
         if int(id)==product.id:
             return cart.get(id)
